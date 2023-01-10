3a9c625631f048f6b844c51b7a31a8abc190418d
SolidSnake <solid@snake>
2023-01-10 14:36:11 -0500

    fix Plutarch is redundant

--- src/AlwaysSucceeds.hs
+++ src/AlwaysSucceeds.hs
@@ -1,12 +1,9 @@
 module AlwaysSucceeds (validator) where
 
-import Plutarch (
-  ClosedTerm,
- )
+import Plutarch.Prelude
 import Plutarch.Api.V2 (
   PValidator,
  )
-import Plutarch.Prelude
 
 validator :: ClosedTerm PValidator
 validator = plam $ \_ _ _ -> popaque $ pconstant True

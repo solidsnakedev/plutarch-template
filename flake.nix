c864b3d8fd2256708bd5bc42200cf6e4e90618eb
SolidSnake <solid@snake>
2023-01-10 14:28:39 -0500

    add plutarch-extra and new liqwid-libs monorepo

--- flake.nix
+++ flake.nix
@@ -3,21 +3,23 @@
 
   nixConfig = {
     extra-experimental-features = [ "nix-command" "flakes" "ca-derivations" ];
-    extra-substituters = [ "https://cache.iog.io" "https://public-plutonomicon.cachix.org" ];
-    extra-trusted-public-keys = [ "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ=" "public-plutonomicon.cachix.org-1:3AKJMhCLn32gri1drGuaZmFrmnue+KkKrhhubQk/CWc=" ];
+    extra-substituters = [ "https://cache.iog.io" ];
+    extra-trusted-public-keys = [ "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ=" ];
     allow-import-from-derivation = "true";
     bash-prompt = "\\[\\e[0;92m\\][\\[\\e[0;92m\\]nix develop:\\[\\e[0;92m\\]\\w\\[\\e[0;92m\\]]\\[\\e[0;92m\\]$ \\[\\e[0m\\]";
+    max-jobs = "auto";
+    auto-optimise-store = "true";
   };
 
   inputs = {
     tooling.url = "github:mlabs-haskell/mlabs-tooling.nix";
     plutarch.url = "github:Plutonomicon/plutarch";
     ply.url = "github:mlabs-haskell/ply?ref=master";
-    plutus-simple-model.url = "github:mlabs-haskell/plutus-simple-model/fa0aa0382ecabf6fcbef8c7b9c35d85ff7b57765";
-
+    plutus-simple-model.url = "github:mlabs-haskell/plutus-simple-model";
+    liqwid-libs.url = "github:Liqwid-Labs/liqwid-libs";
   };
 
-  outputs = inputs@{ self, tooling, plutus-simple-model, plutarch, ply }: tooling.lib.mkFlake { inherit self; }
+  outputs = inputs@{ self, tooling, plutus-simple-model, plutarch, ply, liqwid-libs }: tooling.lib.mkFlake { inherit self; }
     {
       imports = [
         (tooling.lib.mkHaskellFlakeModule1 {
@@ -31,8 +33,13 @@
           project.extraHackage = [
             "${plutus-simple-model}"
             "${plutarch}"
+            "${plutarch}/plutarch-extra"
             "${ply}/ply-core"
             "${ply}/ply-plutarch"
+            "${liqwid-libs}/liqwid-plutarch-extra"
+            "${liqwid-libs}/plutarch-quickcheck"
+            "${liqwid-libs}/plutarch-unit"
+            "${liqwid-libs}/liqwid-script-export"
           ];
         })
       ];

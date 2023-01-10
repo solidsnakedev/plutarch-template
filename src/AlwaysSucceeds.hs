79f0c149bab6129329a49e07d26ce3ca920abcf1
SolidSnake <solid@snake>
2023-01-10 14:37:21 -0500

    run ,format

--- src/AlwaysSucceeds.hs
+++ src/AlwaysSucceeds.hs
@@ -1,9 +1,9 @@
 module AlwaysSucceeds (validator) where
 
-import Plutarch.Prelude
 import Plutarch.Api.V2 (
   PValidator,
  )
+import Plutarch.Prelude
 
 validator :: ClosedTerm PValidator
 validator = plam $ \_ _ _ -> popaque $ pconstant True

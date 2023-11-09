84962a8202238596cbc8a0395d99026eff0b59cc
solidsnakedev <jona.ca.eng@gmail.com>
2023-11-09 11:33:33 -0500

    feat: add new liqwid-nix since mlabs-tooling is deprecated

--- src/AlwaysSucceeds.hs
+++ src/AlwaysSucceeds.hs
@@ -1,9 +1,11 @@
 module AlwaysSucceeds (validator) where
 
 import Plutarch.Api.V2 (
-  PValidator,
+    PValidator,
  )
 import Plutarch.Prelude
 
 validator :: ClosedTerm PValidator
-validator = plam $ \_ _ _ -> popaque $ pconstant True
+validator = plam $ \_ _ _ ->
+    popaque $
+        pconstant True

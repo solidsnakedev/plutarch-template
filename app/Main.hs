05f7ff7700cd4950f8237113c97f4a7991bbf9cc
solidsnakedev <jona.ca.eng@gmail.com>
2023-11-13 14:41:32 -0500

    feat: add better compilation, format files, reorder cabal file

--- app/Main.hs
+++ app/Main.hs
@@ -1,13 +1,62 @@
 module Main (main) where
 
 import AlwaysSucceeds qualified
-import Data.Default (
-    def,
+
+import Cardano.Binary qualified as CBOR
+import Data.Aeson (KeyValue ((.=)), object)
+import Data.Aeson.Encode.Pretty (encodePretty)
+import Data.Bifunctor (
+    first,
  )
-import Ply.Plutarch (
-    writeTypedScript,
+import Data.ByteString.Base16 qualified as Base16
+import Data.ByteString.Lazy qualified as LBS
+import Data.Text (
+    Text,
+    pack,
  )
+import Data.Text.Encoding qualified as Text
+import Plutarch (
+    Config (Config),
+    TracingMode (DoTracing),
+    compile,
+ )
+import Plutarch.Evaluate (
+    evalScript,
+ )
+import Plutarch.Prelude
+import Plutarch.Script (Script, serialiseScript)
+import PlutusLedgerApi.V2 (
+    Data,
+    ExBudget,
+ )
+import "liqwid-plutarch-extra" Plutarch.Extra.Script (
+    applyArguments,
+ )
+
+encodeSerialiseCBOR :: Script -> Text
+encodeSerialiseCBOR = Text.decodeUtf8 . Base16.encode . CBOR.serialize' . serialiseScript
+
+evalT :: ClosedTerm a -> Either Text (Script, ExBudget, [Text])
+evalT x = evalWithArgsT x []
+
+evalWithArgsT :: ClosedTerm a -> [Data] -> Either Text (Script, ExBudget, [Text])
+evalWithArgsT x args = do
+    cmp <- compile (Config DoTracing) x
+    let (escr, budg, trc) = evalScript $ applyArguments cmp args
+    scr <- first (pack . show) escr
+    pure (scr, budg, trc)
+
+writePlutusScript :: String -> FilePath -> ClosedTerm a -> IO ()
+writePlutusScript title filepath term = do
+    case evalT term of
+        Left e -> putStrLn (show e)
+        Right (script, _, _) -> do
+            let
+                scriptType = "PlutusScriptV2" :: String
+                plutusJson = object ["type" .= scriptType, "description" .= title, "cborHex" .= encodeSerialiseCBOR script]
+                content = encodePretty plutusJson
+            LBS.writeFile filepath content
 
 main :: IO ()
 main = do
-    writeTypedScript def "test" "./alwaysSucceeds.plutus" AlwaysSucceeds.validator
+    writePlutusScript "AlwaysSucceeds" "./AlwaysSucceeds.json" AlwaysSucceeds.validator
